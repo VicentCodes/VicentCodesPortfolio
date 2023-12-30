2d9590adfe1a456f2f6979c2a3ad4a94e0ff8e98
VicentCodes <vicentcode@gmail.com>
2023-12-29 22:17:40 -0600

    add @vercel/analytics

--- src/routers/index.js
+++ src/routers/index.js
@@ -14,9 +14,6 @@ const { SitemapStream, streamToPromise } = require("sitemap");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const analytics = require('@vercel/analytics');
-const analyticsHandler = analytics();
-
 const secretKey = process.env.SESSION_SECRET || "tu_secreto_predeterminado";
 
 
@@ -47,8 +44,6 @@ admin.initializeApp({
 
 const storage = admin.storage().bucket();
 const router = express.Router();
-
-router.use(analyticsHandler);
 
 // Utility function to determine content type based on file extension
 function getContentType(filename) {
